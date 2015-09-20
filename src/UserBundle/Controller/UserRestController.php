1c0355b524eeb3058e5b48728a8e8278b7d3e559
ragoupady <sagadevin.ragoupady@gmail.com>
2015-09-20 20:46:30 +0200

    Ajout du service de recupération d'un user OK

--- src/UserBundle/Controller/UserRestController.php
+++ src/UserBundle/Controller/UserRestController.php
@@ -4,18 +4,36 @@ namespace UserBundle\Controller;
 
 use FOS\RestBundle\Controller\Annotations\View;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use FOS\RestBundle\Controller\Annotations\Get;
+use FOS\RestBundle\Controller\Annotations\Post;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 //use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class UserRestController extends Controller
 {
 
-  public function getUsersAction(){
-    $users = $this->getDoctrine()->getRepository('UserBundle:User')->findAll();
+  /**
+   * @Post ("/user")
+   */
+  public function postUserAction(Request $request){
+
+  	$username = $request->request->get('username');
+  	$password = $request->request->get('password');  // il faut régler le pb du mot de pass crypté et salt
+
+  	$password ="rogi{g8sg9ivlcz4sw4go8o84488wg8cws8o}";
     
-    if(!is_object($users)){
+    $user = $this->getDoctrine()->getRepository('UserBundle:User')->findBy(array(
+                                                                                  'username' => $username,
+    																		                                          'password' => $password)
+ 																	                                        );
+
+    if(!is_object($user)){
       //throw $this->createNotFoundException();
     }
 
-    return $users;
+    return $user;
+
   }
 }
